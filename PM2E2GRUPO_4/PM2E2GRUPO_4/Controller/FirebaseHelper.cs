e2fd5efaa30d8333239dbc9c36226b08d85a94eb
User <User@DESKTOP-TO385ES>
2021-11-19 01:51:51 -0800

    Add project files.

---
+++ PM2E2GRUPO_4/PM2E2GRUPO_4/Controller/FirebaseHelper.cs
@@ -0,0 +1,67 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using Firebase.Storage;
+using PM2E2GRUPO_4.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM2E2GRUPO_4.Controller
+{
+   public class FirebaseHelper
+    {
+        FirebaseClient firebase = new FirebaseClient("https://apis-movil-2-default-rtdb.firebaseio.com/ExamenBD");
+        static FirebaseStorage stroageImage = new FirebaseStorage("apis-movil-2.appspot.com");
+
+        public async Task AddPerson(String personId, string name, string descripcion, string DescripcionCorta, string img)
+        {
+
+            await firebase
+              .Child("ExamenBD")
+              .PostAsync(new Ubicaciones() { latitud = personId, longitud = name, descripcion= descripcion, DescripcionCorta = DescripcionCorta, img= img });
+        }
+
+        public static async Task<string> UploadFile(Stream fileStream, string fileName)
+        {
+            var imageUrl = await stroageImage
+                .Child("ExamenImagenes")
+                .Child(fileName)
+                .PutAsync(fileStream);
+            return imageUrl;
+        }
+
+        public async Task AddUbicacion(string latitud, string longitud, string descripcion)
+        {
+            Ubicaciones i = new Ubicaciones() { latitud = latitud, longitud = longitud, descripcion = descripcion };
+            await firebase.Child("ExamenBD")
+                .PostAsync(i);
+        }
+
+        public ObservableCollection<Ubicaciones> getUbicaciones()
+        {
+            var itemData = firebase.Child("ExamenBD").AsObservable<Ubicaciones>()
+                .AsObservableCollection();
+            return itemData;
+        }
+        public async Task DeleteUbicacion(string latitud, string longitud, string descripcion)
+        {
+            var toDeleteUbicacion = (await firebase.Child("ExamenBD")
+                .OnceAsync<Ubicaciones>()).FirstOrDefault(a => a.Object.latitud == latitud
+                || a.Object.longitud == longitud || a.Object.descripcion == descripcion);
+            await firebase.Child("ExamenBD").Child(toDeleteUbicacion.Key).DeleteAsync();
+        }
+        public async Task UpdateUbicaciones(string latitud, string longitud, string descripcion)
+        {
+            var toUpdateUbicaciones = (await firebase.
+                Child("ExamenBD").OnceAsync<Ubicaciones>()).FirstOrDefault
+                (a => a.Object.latitud == latitud
+                || a.Object.longitud == longitud || a.Object.descripcion == descripcion);
+            Ubicaciones u = new Ubicaciones() { latitud = latitud, longitud = longitud, descripcion = descripcion };
+            await firebase.Child("ExamenBD").Child(toUpdateUbicaciones.Key).PutAsync(u);
+        }
+    }
+}
---
+++ PM2E2GRUPO_4/PM2E2GRUPO_4/Views/UbicacionesDetailsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using PM2E2GRUPO_4.Controller;
+using PM2E2GRUPO_4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E2GRUPO_4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UbicacionesDetailsPage : ContentPage
+    {
+        FirebaseHelper services;
+        public UbicacionesDetailsPage(Ubicaciones ubicaciones)
+        {
+            InitializeComponent();
+            BindingContext = ubicaciones;
+            services = new FirebaseHelper();
+        }
+        public async void BtnDelete_Ubicaciones(object sender, ItemTappedEventArgs args)
+        {
+            await services.DeleteUbicacion(latitud.Text, longitud.Text, descripcion.Text);
+            await Navigation.PushAsync(new UbicacionesPage());
+
+        }
+        public async void BtnUpdate_Ubicaciones(object sender, ItemTappedEventArgs args)
+        {
+            await services.UpdateUbicaciones(latitud.Text, longitud.Text, descripcion.Text);
+            await Navigation.PushAsync(new UbicacionesPage());
+
+        }
+    }
+}
